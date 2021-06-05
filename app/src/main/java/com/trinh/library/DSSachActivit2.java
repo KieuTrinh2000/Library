6e0e9cf32f25b4c1a84f5b0f73203424ee9acfa9
Admin <kieu35374@gmail.com>
2021-06-06 00:30:40 +0700

    OK part 2

---
+++ app/src/main/java/com/trinh/library/DSSachActivit2.java
@@ -0,0 +1,14 @@
+package com.trinh.library;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DSSachActivit2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ds_sach);
+    }
+}
